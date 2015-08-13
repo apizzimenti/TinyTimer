2e86b3f1d4e3d1a2b3e0e7ff8bbe45980736e507
apizzimenti <pizzimentianthony@gmail.com>
2015-08-13 02:51:10 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+###TinyTimer
+My dad needed a timer for work, so I made him a timer. It even has its own site.
---
+++ timer.js
@@ -0,0 +1,44 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*jslint plusplus: true*/
+/*jslint node: true*/
+
+$(document).ready(function () {
+    "use strict";
+    var seconds, timer, location = document.getElementById("location"), $start = $("#start"), counter = 0,
+        $stop = $("#stop"), $clear = $("#clear"), flag = 0, time = document.getElementById("time"),
+        iterations = document.getElementById("iterations");
+
+    $start.click(function () {
+        seconds = parseInt(time.value, 10);
+        
+        if (flag > 0) {
+            seconds = parseInt(location.innerHTML, 10);
+        }
+        
+        timer = window.setInterval(function () {
+            seconds--;
+            location.innerHTML = seconds;
+            
+            if (seconds <= 0) {
+                seconds = parseInt(time.value, 10);
+                flag = 0;
+                counter++;
+                iterations.innerHTML = counter;
+            }
+            
+        }, 1000);
+    });
+
+    $stop.click(function () {
+        clearInterval(timer);
+        flag++;
+    });
+    
+    $clear.click(function () {
+        clearInterval(timer);
+        location.innerHTML = 0;
+        flag = 0;
+        iterations.innerHTML = 0;
+    });
+});
